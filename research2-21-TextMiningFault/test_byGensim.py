3b26323f5076091d2d2ede551c29e9c42ee8f09b
yanglinchao <xinx_yang@163.com>
2021-08-04 10:38:45 +0800

    Update test_byGensim.py

--- research2-21-TextMiningFault/test_byGensim.py
+++ research2-21-TextMiningFault/test_byGensim.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import jieba
 from gensim import corpora, models
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 # 导入数据
 data_tf = pd.read_csv('原始数据.csv',
@@ -24,21 +25,5 @@ data_cut_ph = data_tf.ph
 list_result_cut_ph = simpleWordCut(pd.Series.tolist(data_cut_ph.astype(str)), stopwords, cut_all=False, HMM=False)
 
 # 提取TF-IDF特征向量
-list_tfidf = tfidfGensim(list_result_cut_ph)
+list_tfidf = tfidf_Gensim(list_result_cut_ph)
 
-dictionary = corpora.Dictionary(list_result_cut_ph)
-corpus = [dictionary.doc2bow(doc) for doc in list_result_cut_ph]
-
-lda_model = models.LdaModel(corpus, id2word=dictionary, num_topics=3)
-corpus_lda = [lda_model[doc] for doc in corpus]
-result = []
-for i in range(len(corpus_lda)):
-    a = corpus_lda[i]
-    single_result = []
-    for j in range(len(a)):
-        single_result.append(a[j][1])
-    result.append(single_result)
-print(result)
-
-word2vec_result = WORD2VEC_Gensim(list_result_cut_ph)
-result = featureVec_2vec(list_result_cut_ph, word2vec_result, method="mean")
