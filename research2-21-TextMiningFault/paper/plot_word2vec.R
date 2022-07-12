ff42cc5c6a9064ed9e88e730d5a80ce4bf1f6bce
yanglinchao <xinx_yang@163.com>
2022-07-13 00:42:11 +0800

    Update plot_word2vec.R

--- research2-21-TextMiningFault/paper/plot_word2vec.R
+++ research2-21-TextMiningFault/paper/plot_word2vec.R
@@ -7,15 +7,15 @@ windowsFonts(RMN = windowsFont("Times New Roman"))
 data <- read.csv("result_handle.csv")
 names(data) <- c("result", "index", "cut_type", "vectorsize", "algorithm", "paramter", "type", "k")
 data$type <- factor(data$type, levels = c("control1", "control2", "goal"),
-                    labels = c("MC1", "MC2", "M"), ordered = TRUE)
+                    labels = c("Model 1", "Model 2", "Model 3"), ordered = TRUE)
 data <- as_tibble(data)
 
+plot_size <- 35
 
 # »­word2vec
 # Accuracy
 plot_index <- "Accuracy"
 plot_cut_type <- "Word2Vec"
-plot_size <- 20
 data_plot <- data %>% filter(index == plot_index & cut_type == plot_cut_type)
 bar <- ggplot(data = (data_plot), aes(x = vectorsize, y = result, fill = type)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -38,7 +38,6 @@ ggsave(bar, dpi = 1200, height = 8, width = 7, filename = paste("C:/Users/ylc/De
 # Precision
 plot_index <- "Precision"
 plot_cut_type <- "Word2Vec"
-plot_size <- 20
 data_plot <- data %>% filter(index == plot_index & cut_type == plot_cut_type)
 bar <- ggplot(data = (data_plot), aes(x = vectorsize, y = result, fill = type)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -61,7 +60,6 @@ ggsave(bar, dpi = 1200, height = 8, width = 7, filename = paste("C:/Users/ylc/De
 # Recall
 plot_index <- "Recall"
 plot_cut_type <- "Word2Vec"
-plot_size <- 20
 data_plot <- data %>% filter(index == plot_index & cut_type == plot_cut_type)
 bar <- ggplot(data = (data_plot), aes(x = vectorsize, y = result, fill = type)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -84,7 +82,6 @@ ggsave(bar, dpi = 1200, height = 8, width = 7, filename = paste("C:/Users/ylc/De
 # F1-Score
 plot_index <- "F1-Score"
 plot_cut_type <- "Word2Vec"
-plot_size <- 20
 data_plot <- data %>% filter(index == plot_index & cut_type == plot_cut_type)
 bar <- ggplot(data = (data_plot), aes(x = vectorsize, y = result, fill = type)) +
   geom_bar(stat = "identity", position = "dodge") +
